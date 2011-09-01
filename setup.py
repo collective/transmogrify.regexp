9aaf8c5a797fb36811ce61e30cefd7651b305a60
Alex Clark <aclark@aclark.net>
2011-08-31 23:18:05 -0400

    Fix syntax

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     entry_points={
         'z3c.autoinclude.plugin': 'target = transmogrify',
-    }
+    },
     name='transmogrify.regexp',
     namespace_packages=['transmogrify'],
     packages=find_packages(),
