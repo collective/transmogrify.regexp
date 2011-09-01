b5952c5724a95908fc201e7ab9340f97814827c3
Alex Clark <aclark@aclark.net>
2011-08-31 23:09:53 -0400

    Add ver

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='transmogrify.regexp',
-    namespace_packages=['transmogrify'],
-    packages=find_packages(),
     entry_points={
         'z3c.autoinclude.plugin': 'target = transmogrify',
     }
+    name='transmogrify.regexp',
+    namespace_packages=['transmogrify'],
+    packages=find_packages(),
+    version='0.1.0',
 )
